8319a7f301f0d46dd3b7d20f18a2a5957af04ced
Pawan Jheeta <pjheeta@gmail.com>
2020-05-10 00:29:47 -0700

    Changes 2, added a static classs

---
+++ Class/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    class fart
+    {
+            public static void sayHI (string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
--- Class/Program.cs
+++ Class/Program.cs
@@ -6,10 +6,17 @@ namespace Class
     public class Person
     {
         public string Name;
+        public static int PeopleCount = 0;
 
         public void Introduce()
         {
             Console.WriteLine("Hi, my name is " + Name);
+
+        }    
+        
+        public void Introduce2(string newPerson)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", newPerson, Name);
 
         }
     }
@@ -23,7 +30,9 @@ namespace Class
 
             Console.Write("What is your name? ");
             banana.Name = Console.ReadLine();
-            banana.Introduce();
+            //banana.Introduce();
+            banana.Introduce2("Pawan");
+            fart.sayHI("Tutty");
 
         }
     }
