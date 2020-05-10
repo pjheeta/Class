f1c62848891efc310b9a5b4fdcb6cdfd753a0424
Pawan Jheeta <pjheeta@gmail.com>
2020-05-09 23:08:02 -0700

    Basic Class functionality

--- Class/Program.cs
+++ Class/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Class
 {
+
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, my name is " + Name);
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person();
+            var banana = new Person();
+            //You can use 'var' incase your class is big, or if you're lazy
+
+            Console.Write("What is your name? ");
+            banana.Name = Console.ReadLine();
+            banana.Introduce();
+
         }
     }
 }
